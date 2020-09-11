1a20e97a8beebf319005596b33f0e27491c6f5a0
Дмитрий Ураков <rivozent@outlook.com>
2020-09-11 17:33:20 +0800

    Release

---
+++ Numbers/Numbers/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReceiptNumbers RN = new ReceiptNumbers();
+            RN.getLine();
+            Console.ReadKey();
+        }
+    }
+
+    class ReceiptNumbers
+    {
+        string numLine;
+        int[] nums;
+        int resultSum = 0; int resultMul = 1;
+        public void getLine()
+        {
+            Console.WriteLine("Enter the numbers, separate by space button");
+            numLine = Console.ReadLine();
+            arrayMaker();
+        }
+        void arrayMaker()
+        {
+            nums = numLine.Split(' ').Select(str => int.Parse(str)).ToArray();
+            getResult();
+        }
+        void getResult()
+        {
+            Processing processing = new Processing();
+            resultSum = processing.Sum(ref nums);
+            Console.WriteLine("Total Sum: " + resultSum);
+            resultMul = processing.Mul(ref nums);
+            Console.WriteLine("Total Multiplication: " + resultMul);
+        }
+    }
+    class Processing
+    {
+        public int Sum(ref int[] nums)
+        {
+            int result = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result += nums[i];
+            }
+            return result;
+        }
+        public int Mul(ref int[] nums)
+        {
+            int result = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result *= nums[i];
+            }
+            return result;
+        }
+    }
+}
